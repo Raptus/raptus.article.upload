bf673b27cb22ab5ea28264a2b9be3a9eb9ab9d4b
Jonas Wagner <jwagner@raptus.com>
2012-11-02 01:52:29 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b3'
 setup(name='raptus.article.upload',
       version=version,
       description="Provides multiupload functionality for articles using collective.uploadify",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "MANUAL.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -17,7 +17,7 @@ setup(name='raptus.article.upload',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/raptus.article/raptus.article.upload',
+      url='https://github.com/Raptus/raptus.article.upload',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['raptus', 'raptus.article'],
